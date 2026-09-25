0cc25055e2e28b152457e63c57240819b4b56902
ruhulaminislam <abmdruhulamin@gmail.com>
2026-09-25 19:22:35 +0600

    porject setup

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.magnific.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  }
+};
+
+export default nextConfig;
